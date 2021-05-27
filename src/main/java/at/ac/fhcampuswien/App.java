55978520942aa18e7c258198d83c420060ea4787
tim-van-dyck-fh-campus-wien <tim.van.dyck@stud.fh-campuswien.ac.at>
2021-05-27 18:39:56 +0200

    implemented check pwd length

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -4,8 +4,5 @@ public class App {
     public static void main(String[] args){
 
     }
-    public static boolean checkPassword(String pwd){
 
-        return false;
-    }
 }
--- src/main/java/at/ac/fhcampuswien/PasswordValidator.java
+++ src/main/java/at/ac/fhcampuswien/PasswordValidator.java
@@ -1,7 +1,13 @@
 package at.ac.fhcampuswien;
 
 public class PasswordValidator {
-    public boolean checkPassword(String pwd){
+    public  boolean checkPassword(String pwd){
+        if(checkPwdLength(pwd)){
+            return true;
+        }
         return false;
+    }
+    public boolean checkPwdLength(String pwd){
+        return (pwd.length()>=8)&&(pwd.length()<=25);
     }
 }
