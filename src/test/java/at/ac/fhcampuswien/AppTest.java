afeb729c00835ad4d33a341596d6ce7f9c80a89c
tim-van-dyck-fh-campus-wien <tim.van.dyck@stud.fh-campuswien.ac.at>
2021-05-27 17:45:12 +0200

    Initial commit

---
+++ src/test/java/at/ac/fhcampuswien/AppTest.java
@@ -0,0 +1,10 @@
+package at.ac.fhcampuswien;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class AppTest {
+    @Test
+    public void myTest(){
+        assertTrue(true);
+    }
+}
